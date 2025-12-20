b53f389e24237867e2054d9212d9966eea27bc12
shreyansh28801 <shreyanshpatel282001@gmail.com>
2025-12-20 16:06:42 +0530

    simple values

---
+++ 2_simple_values/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	//int
+	fmt.Println(1)
+	//string
+	fmt.Println("Apple")
+	//bool
+	fmt.Println(true)
+	//floats
+	fmt.Println(10.4)
+	fmt.Println(7.0/3.0)
+}
