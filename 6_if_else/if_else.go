caaaaa79f2b73ef40d43dfdb6c4f818d7d18b477
shreyansh28801 <shreyanshpatel282001@gmail.com>
2025-12-21 12:35:59 +0530

    if_else

---
+++ 6_if_else/if_else.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	// age := 18
+
+	// if age<=15 {
+	// 	fmt.Println("preson is child")
+	// } else if age>=16 && age <=18 {
+	// 	fmt.Println("person is more close to adult")
+	// } else {
+	// 	fmt.Println("person is adult")
+	// }
+
+	//variable declaraion inside 'if'
+	if age:=10;age >= 18 {
+		fmt.Println("person is adult ", age)
+	} else if age >= 12 {
+		fmt.Println("person is teenager")
+	}
+
+	//go doesn't have ternary operator
+}
